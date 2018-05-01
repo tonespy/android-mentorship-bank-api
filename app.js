214a7bcde295ee6ab48dafd15c9b2bc75b8afae9
Abubakar Oladeji <abubakar.oladeji@gmail.com>
2018-05-01 04:43:03 +0100

    Revert "* Removed Riby name"
    This reverts commit 9da0423342b4d81ce16b02034c95772f65659cc9.

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ require('./config/routes')(app)
 
 // Setup a default catch-all route that sends back a welcome message in JSON format.
 app.get('*', (req, res) => res.status(200).send({
-  message: 'Welcome to the beginning of Android Mentorship Banking API.'
+  message: 'Welcome to the beginning of riby.'
 }))
 
 module.exports = app
